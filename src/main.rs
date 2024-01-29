cda29adc9d87e72840c6f357a31b050a0e4d23b4
bendn <bend.n@outlook.com>
2024-01-30 04:58:55 +0700

    proper

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ use mindus::{
 fn main() -> ExitCode {
     let mut a = std::env::args().skip(1);
     let tags: Vec<String> = a.by_ref().take_while(|x| x != "--").collect();
+    fn fmt_tags(t: &[String]) -> String {
+        if let [x, rest @ ..] = t {
+            use std::fmt::Write;
+            let mut s = format!("[\"{x}\"");
+            for elem in rest {
+                write!(s, ",\"{elem}\"").unwrap();
+            }
+            write!(s, "]").unwrap();
+            s
+        } else {
+            String::from("[]")
+        }
+    }
+    let tags = fmt_tags(&tags);
     for arg in a {
         let Ok(x) = std::fs::read(&arg) else {
             comat::cprintln!("{arg:red} is not readable");
@@ -17,7 +31,8 @@ fn main() -> ExitCode {
             comat::cprintln!("{arg:red} is not a schematic");
             return ExitCode::FAILURE;
         };
-        x.tags.insert("labels".to_string(), format!("{tags:?}"));
+
+        x.tags.insert("labels".to_string(), tags.clone());
         let mut out = DataWrite::default();
         x.serialize(&mut out).unwrap();
         std::fs::write(arg, out.consume()).unwrap();
